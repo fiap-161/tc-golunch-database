977a898f083c44c4eb2829cdcf360e56740d8d91
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2026-01-09 16:27:49 -0300

    remove duplicated variables

--- variables.tf
+++ variables.tf
@@ -8,15 +8,3 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
-
-variable "core_db_username" {
-  description = "Core Service database username"
-  type        = string
-  default     = "golunch_core_user"
-}
-
-variable "core_db_password" {
-  description = "Core Service database password"
-  type        = string
-  sensitive   = true
-}
