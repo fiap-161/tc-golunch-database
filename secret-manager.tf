24876b11f9f68c44d8cb3d3e5799418bcf797a49
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2026-01-23 14:09:56 -0300

    add secrets for core db

--- secret-manager.tf
+++ secret-manager.tf
@@ -7,3 +7,13 @@ resource "aws_secretsmanager_secret_version" "golunch_db_url" {
   secret_id     = aws_secretsmanager_secret.golunch_db_url.id
   secret_string = aws_db_instance.golunch_postgres.address
 }
+
+resource "aws_secretsmanager_secret" "golunch_core_db_url" {
+  name        = "golunch/core-db-url"
+  description = "Endereço do banco de dados do Core Service"
+}
+
+resource "aws_secretsmanager_secret_version" "golunch_core_db_url" {
+  secret_id     = aws_secretsmanager_secret.golunch_core_db_url.id
+  secret_string = aws_db_instance.golunch_core_postgres.address
+}
